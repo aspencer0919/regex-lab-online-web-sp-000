54a130e5fef6daaf042283407bf9fca15a886dfb
Adam Spencer <aspencer0919@gmail.com>
2020-03-18 22:37:46 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,7 +25,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/([0-9] *){10}|\([0-9]{3}\)\d{3}-\d{4}|\([0-9]{3}\)\d{7}/)
+  if phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b))
   #  binding.pry
     return true
   end
